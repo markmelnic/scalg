708c1e2fc3d91d2d01a62ec887f6851d2b196c28
Mark <markhappy200@gmail.com>
2020-05-01 13:27:00 +0300

    CSV Reader implemented

--- scoring_alg.cpp
+++ scoring_alg.cpp
@@ -1,8 +1,32 @@
 
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ifstream in("input.csv");
+    vector<vector<double>> fields;
+    if (in) {
+        string line;
+        while (getline(in, line)) {
+            stringstream sep(line);
+            string field;
+            fields.push_back(vector<double>());
+            while (getline(sep, field, ',')) {
+                fields.back().push_back(stod(field));
+            }
+        }
+    }
+    for (auto row : fields) {
+        for (auto field : row) {
+            cout << field << ' ';
+        }
+        cout << '\n';
+    }
 }
