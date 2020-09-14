7b100096d0f3342074a6d2f557a92925c28acaf9
Mark Melnic <markhappy200@gmail.com>
2020-09-14 10:04:56 +0200

    Started package creation
    - added setup.py
    
    - created MANIFEST.in
    
    - moved scalg

---
+++ setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="Scoring-Algorithm",
+    version="1.0.0",
+    description="Analyse data file using a range based procentual proximity algorithm and calculate the linear maximum likelihood estimation.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/markmelnic/Scoring-Algorithm",
+    author="Mark Melnic",
+    author_email="commerce.markmelnic@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["scalg"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+        ]
+    },
+)
