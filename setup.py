f3df9593de1bf8aa1407b0d16d6f39a822302d11
Mark Melnic <markhappy200@gmail.com>
2020-09-14 14:03:32 +0200

    Updated setup to find packages

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
     ],
-    packages=['scalg'],
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     zip_safe=False,
     include_package_data=True,
     install_requires=[],
