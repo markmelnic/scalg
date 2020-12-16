e7d4753c2a6125318ad79e28e8558df3dac375ba
Mark Melnic <markhappy200@gmail.com>
2020-12-16 23:40:06 +0100

    Version 0.1.5.1 - removed string args and added bools instead - removed try except where possible because those are expensive - moved data return statements to bottom to avoid further confusion

--- scalg/scalg.py
+++ scalg/scalg.py
@@ -1,4 +1,4 @@
-def score(source_data: list, weights: list, *args) -> list:
+def score(source_data: list, weights: list, get_scores=False, get_score_lists=False) -> list:
     """Analyse data file using a range based percentual proximity
     algorithm and calculate the linear maximum likelihood estimation.
 
@@ -9,25 +9,21 @@ def score(source_data: list, weights: list, *args) -> list:
             1 if higher values have higher weight in the data set
 
     Optional args:
-        "score_lists" (str): Returns a list with lists of each column scores.
-        "scores" (str): Returns only the final scores.
+        get_scores (bool): Returns only the final scores.
+        get_score_lists (bool): Returns a list with lists of each column scores.
 
     Raises:
-        ValueError: Weights can only be either 0 or 1 (int)
+        ValueError: Weights can only be either 0 or 1
 
     Returns:
         list: Source data with the score of the set appended at as the last element.
     """
 
     # getting data
-    data_lists = []
+    data_lists = [[] for i in source_data]
     for item in source_data:
         for i, val in enumerate(item):
-            try:
-                data_lists[i].append(float(val))
-            except IndexError:
-                data_lists.append([])
-                data_lists[i].append(float(val))
+            data_lists[i].append(float(val))
 
     # calculating price score
     score_lists = []
@@ -38,26 +34,22 @@ def score(source_data: list, weights: list, *args) -> list:
         score = []
         if weight == 0:
             for item in dlist:
-                try:
+                if not maxd - mind == 0:
                     score.append(1 - ((item - mind) / (maxd - mind)))
-                except ZeroDivisionError:
+                else:
                     score.append(1)
 
         elif weight == 1:
             for item in dlist:
-                try:
+                if not maxd - mind == 0:
                     score.append((item - mind) / (maxd - mind))
-                except ZeroDivisionError:
+                else:
                     score.append(0)
 
         else:
             raise ValueError("Invalid weight of %f provided" % (weight))
 
         score_lists.append(score)
-
-    # return score lists
-    if "score_lists" in args:
-        return score_lists
 
     # initialize final scores
     final_scores = [0 for i in range(len(score_lists[0]))]
@@ -67,14 +59,17 @@ def score(source_data: list, weights: list, *args) -> list:
         for j, ele in enumerate(slist):
             final_scores[j] = final_scores[j] + ele
 
-    # return only scores
-    if "scores" in args:
-        return final_scores
-
     # append scores to source data
     for i, ele in enumerate(final_scores):
         source_data[i].append(ele)
 
+    # return score lists
+    if get_score_lists:
+        return score_lists
+    # return only scores
+    if get_scores:
+        return final_scores
+    # return source data with appended scores
     return source_data
 
 
@@ -101,7 +96,7 @@ def score_columns(source_data: list, columns: list, weights: list) -> list:
     if len(weights) > len(columns):
         weights = [weights[item] for item in columns]
 
-    for i, sc in enumerate(score(temp_data, weights, "scores")):
+    for i, sc in enumerate(score(temp_data, weights, get_scores=True)):
         source_data[i].append(sc)
 
     return source_data
